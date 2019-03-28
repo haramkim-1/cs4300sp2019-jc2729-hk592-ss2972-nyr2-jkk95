e5aeb34c058990870e15b15ed4705babc26f247f
Janice Chan <jc2729@cornell.edu>
2019-03-27 21:45:17 -0400

    add vroom images for gif and renamed app

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,7 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "iSight"
+project_name = "Vroom Vroom"
 net_id = "Janice Chan: jc2729, Haram Kim: hk592, Stephanie Shum: ss2972, Nataly Rodriguez: nyr2, Jasmine Kitahara: jkk95"
 
 @irsystem.route('/', methods=['GET'])
