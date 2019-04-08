2f0ed4b0916282ba1bfd1c24455b65a637553d29
Haram Kim <kimharam@gmail.com>
2019-04-08 18:39:11 -0400

    search framework

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -1,12 +1,13 @@
-with open('data/unfiltered_list.json') as json_file:
-	unfiltered_list = json.load(json_file)
+from json import load
 
-    #filter by size
-    def filter_sizes(min_size, max_size, min_price, max_price, car_size, car_price):
-        return car_size >= min_size and car_size <= max_size
+#filter by size
+def filter_sizes(min_size, max_size, min_price, max_price, car_size, car_price):
+    return car_size >= min_size and car_size <= max_size
 
-    min_size = 0
-    max_size = 1
-    min_price = 0
-    max_price = 100000
-    truncated_list_by_size = [x[0] for x in unfiltered_list if filter_sizes(min_size, max_size, min_price, max_price, x[1], x[2])]
+class searcher:
+    def __init__(self):
+        with open('data/unfiltered_list.json') as json_file:
+            self.unfiltered_list = load(json_file)
+
+    def search(self, min_size, max_size, min_price, max_price):
+        truncated_list_by_size = [x[0] for x in self.unfiltered_list if filter_sizes(min_size, max_size, min_price, max_price, x[1], x[2])]
