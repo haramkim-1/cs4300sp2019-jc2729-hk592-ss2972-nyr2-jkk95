80f65425fc414f8b90d3ef3001cdd46689845de5
Haram Kim <kimharam@gmail.com>
2019-04-08 18:27:58 -0400

    misc search controller changes

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,8 @@
 from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from flask import request
+from app.irsystem.models.search import handle_search
 
 project_name = "Vroom Vroom"
 net_id = "Janice Chan: jc2729, Haram Kim: hk592, Stephanie Shum: ss2972, Nataly Rodriguez: nyr2, Jasmine Kitahara: jkk95"
@@ -25,6 +27,8 @@ def do_search():
 	carsize_range = request.args.get("")
 	keywords = request.args.get("")
 	price_range = request.args.get("")
+
+	# convert compact: 0, midsize: 1, large: 2
 	
 	# TODO: do something here
 
