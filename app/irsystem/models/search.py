8f4d3c32ec317bbfdf72c17e729e8140c4c2ea7a
Haram Kim <kimharam@gmail.com>
2019-04-08 18:40:38 -0400

    add keywords argument

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -9,5 +9,5 @@ class searcher:
         with open('data/unfiltered_list.json') as json_file:
             self.unfiltered_list = load(json_file)
 
-    def search(self, min_size, max_size, min_price, max_price):
+    def search(self, min_size, max_size, min_price, max_price, keywords):
         truncated_list_by_size = [x[0] for x in self.unfiltered_list if filter_sizes(min_size, max_size, min_price, max_price, x[1], x[2])]
