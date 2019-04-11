75a24c34cba936f4a396536ec57ac05f436e2a6b
Janice Chan <jc2729@cornell.edu>
2019-04-11 08:48:03 -0400

    fixed styling of frontend keywords

--- app/frontend/src/List.js
+++ app/frontend/src/List.js
@@ -5,7 +5,7 @@ const List = props => (
   
   <ul>
     {
-      props.items.map((item, index) => <Button type="button" style={{margin:'5px'}} key={index} onClick={() => {props.delete(item)}}>{item} <span className="close">x</span></Button>)
+      props.items.map((item, index) => <Button type="button" style={{margin:'10px 5px'}} key={index} onClick={() => {props.delete(item)}}>{item} <span className="close">x</span></Button>)
     }
   </ul>
 );
