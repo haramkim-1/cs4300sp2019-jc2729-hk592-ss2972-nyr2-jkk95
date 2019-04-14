82bf10f41cf4d627adb4d109cd5a584c9fec28e8
nataly_r <nyr2@cornell.edu>
2019-04-14 18:33:50 -0400

    key word box

--- app/frontend/src/List.js
+++ app/frontend/src/List.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './List.css'
 const List = props => (
-  
+
   <ul>
     {
-      props.items.map((item, index) => <Button type="button" style={{margin:'10px 5px'}} key={index} onClick={() => {props.delete(item)}}>{item} <span className="close">x</span></Button>)
+      props.items.map((item, index) => <Button className="keybutton" type="button" style={{margin:'10px 5px',background:"#3f51b5"}} key={index} onClick={() => {props.delete(item)}}>{item} <span className="close">x</span></Button>)
     }
   </ul>
 );
 
 export default List;
-
