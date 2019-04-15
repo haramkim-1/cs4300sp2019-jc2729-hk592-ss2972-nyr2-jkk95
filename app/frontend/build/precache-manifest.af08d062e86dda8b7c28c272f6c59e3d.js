370b9be2a01959630bc179047c1656fe62d8a424
Haram Kim <kimharam@gmail.com>
2019-04-14 22:14:34 -0400

    more heroku commits

---
+++ app/frontend/build/precache-manifest.af08d062e86dda8b7c28c272f6c59e3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a52c9fc01323fdd27d860cb6f76627e8",
+    "url": "/static/media/logo.a52c9fc0.gif"
+  },
+  {
+    "revision": "b1a8d3663ca8ca3e5e99083da22b1913",
+    "url": "/static/media/background.b1a8d366.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ced88f166dfda723b47f",
+    "url": "/static/js/main.7747c447.chunk.js"
+  },
+  {
+    "revision": "80100996bb924b588575",
+    "url": "/static/js/2.f2462b6c.chunk.js"
+  },
+  {
+    "revision": "ced88f166dfda723b47f",
+    "url": "/static/css/main.a04af17b.chunk.css"
+  },
+  {
+    "revision": "80100996bb924b588575",
+    "url": "/static/css/2.05821ec1.chunk.css"
+  },
+  {
+    "revision": "999387c3492526ea7c3d0c409658f295",
+    "url": "/index.html"
+  }
+];
