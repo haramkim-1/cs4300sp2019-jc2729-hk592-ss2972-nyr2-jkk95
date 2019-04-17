2c9ea1271cd3f63d05f4232ad7bb17425a675f57
Stephanie Shum <ss2972@cornell.edu>
2019-04-17 17:05:40 -0400

    Add comments and remove functions that are not longer needed

--- tfidfprocessing.py
+++ tfidfprocessing.py
@@ -13,29 +13,34 @@ import re
 import string
 import pickle
 
+#Some of the following methods were inspired by various homework assignments
+
 def tokenize(text):
+	"""
+	Tokenize the review and remove words that have a length of 2 or smaller
+	"""
 	tokenized_review = re.findall(r'[a-z]+', text.lower())
 	return [x for x in tokenized_review if len(x)>2]
 
 def build_vectorizer(max_features, stop_words, max_df=0.65, min_df=45, norm='l2', tokenizer=tokenize):
-	"""Returns a TfidfVectorizer object with the above preprocessing properties.
-
-	Params: {max_features: Integer,
-			 max_df: Float,
-			 min_df: Float,
-			 norm: String,
-			 stop_words: String}
-	Returns: TfidfVectorizer
+	"""
+	Returns a TfidfVectorizer object with the above preprocessing properties.
 	"""
 	return TfidfVectorizer(max_features=max_features, min_df=min_df, max_df=max_df, stop_words=stop_words, norm=norm, tokenizer=tokenize)
 
 def create_unique_cars_list(data):
+	"""
+	Create a list of unique cars, the unique identifier being their Year_Make_Model
+	"""
 	cars_list = []
 	for car in data:
 		cars_list.append(car['Year_Make_Model'])
 	return cars_list
 
 def create_sizes_list(data, size_rev_idx):
+	"""
+	Create a list of tuples containing the car's Year_Make_Model, size, and MSRP
+	"""
 	size_list = []
 	for car in data:
 		s = size_rev_idx[car['Vehicle Size']]
@@ -43,48 +48,27 @@ def create_sizes_list(data, size_rev_idx):
 
 	return size_list
 
-def build_inverted_index(msgs):
-	inverted_index = {}
-	for index, msg in enumerate(msgs):
-		tokens = msg['Tokenized Reviews']
-		tokens_set = set(tokens)
-		for token in tokens_set:
-			if token not in inverted_index:
-				inverted_index[token] = [(index, tokens.count(token))]
-			else:
-				inverted_index[token].append((index, tokens.count(token)))
-	return inverted_index
-
-def compute_idf(inv_idx, n_docs, min_df=10, max_df_ratio=0.95):
-	idf_dict = {}
-	for term, tf_list in inv_idx.items():
-		df = len(tf_list)
-		if (df >= min_df and df/n_docs <= max_df_ratio):
-			frac = n_docs/(1+df)
-			idf = math.log(frac, 2)
-			idf_dict[term] = idf
-
-	return idf_dict
 
 with open('data/data.json') as json_file:
 	data = list(json.load(json_file).values())
 
 	num_cars = len(data)
-
+	#Append all of the reviews for a car together, and remove numbers
 	for car in data:
 		car['Appended Reviews'] = ""
 		for review in car['reviews']:
 			new_review = (re.sub('[0-9]+', '', review['Review'])).lower()
 			car['Appended Reviews'] = car['Appended Reviews'] + new_review + ' '
-		# car['Tokenized Reviews'] = tokenize(car['Appended Reviews'])
 
 	n_feats = 4000
 	doc_by_vocab = np.empty([len(data), n_feats])
 
+	#Create a tf_idf matrix
 	tfidf_vec = build_vectorizer(n_feats, "english")
 	doc_by_vocab = tfidf_vec.fit_transform([d['Appended Reviews'] for d in data]).toarray()
 	index_to_vocab = {i:v for i, v in enumerate(tfidf_vec.get_feature_names())}
 
+	#Create a list of unique cars
 	unique_cars = create_unique_cars_list(data)
 	cars_reverse_index = {car: i for i, car in enumerate(unique_cars)}
 
@@ -94,10 +78,6 @@ with open('data/data.json') as json_file:
 
 	unfiltered_list = create_sizes_list(data,size_reverse_index)
 
-	# inv_idx = build_inverted_index(data)
-
-	# idf_dict = compute_idf(inv_idx, len(data))
-
 	with open("data/tfidf_vec.pkl", "wb+") as file:
 		pickle.dump(tfidf_vec, file)
 
@@ -105,13 +85,7 @@ with open('data/data.json') as json_file:
 	with open("data/unfiltered_list.pkl", "wb+") as file:
 		pickle.dump(unfiltered_list, file)
 
-	# with open("data/keywords", "w+") as file:
-	# 	pickle.dump(list(index_to_vocab.values()), file)
-
 	np.save("data/doc_by_vocab", doc_by_vocab)
 
 	with open("data/index_to_vocab.pkl", "wb+") as file:
 		pickle.dump(index_to_vocab, file)
-
-	# with open("data/idf_dict", "w+") as file:
-	# 	pickle.dump(idf_dict, file)
