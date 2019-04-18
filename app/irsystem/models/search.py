afeb98904734cddf23278474e21f3cd4cac4d80d
Janice Chan <jc2729@cornell.edu>
2019-04-18 16:39:56 -0400

    add query expansion kmeans, quality tbd

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -25,9 +25,6 @@ def filter_sizes(min_size, max_size, min_price, max_price, car_size, car_price):
 	return car_size >= min_size and car_size <= max_size and car_price >= min_price and car_price <= max_price
 #cosine similarity
 def get_sim(car, q):
-	print(car)
-	print('******')
-	print(q)
 	numerator = np.dot(car, q)
 	norm1 = np.linalg.norm(car)
 	norm2 = np.linalg.norm(q)
@@ -39,12 +36,14 @@ class Searcher:
 
 		with open(join(data_path, 'unfiltered_list.pkl'), 'rb') as unfiltered, \
 			open(join(data_path, 'index_to_vocab.pkl'), 'rb') as itv, \
-			open(join(data_path, 'data.json')) as all_data:
+			open(join(data_path, 'data.json')) as all_data, \
+			open(join(data_path, 'query_expansion_words.pkl'), 'rb') as query_expansion_file:
 			self.all_data = load(all_data)
 			self.unfiltered_list = pickle.load(unfiltered)
 			self.index_to_vocab = pickle.load(itv)
 			self.vocab_to_index = {self.index_to_vocab[k]:int(k) for k in self.index_to_vocab}
 			self.cars_reverse_index = {car[0]: i for i, car in enumerate(self.unfiltered_list)}
+			self.query_expansion_words = pickle.load(query_expansion_file)
 
 		n_feats = 4000
 		# self.doc_by_vocab = np.empty([len(self.all_data), n_feats])
@@ -69,16 +68,28 @@ class Searcher:
 		# for t in query:
 		#	 print("\t" + t)
 		#	 tf_idf_query[self.vocab_to_index[t]] = self.idf_dict[t]
-		query = " ".join(query)
-		tf_idf_query = self.tfidf_vec.transform([query]).toarray()[0]
+
+		# query expansion
+		query = query[0].split(',')
+		
+		expanded_query = []
+		for term in query:
+			in_cluster = False
+			for cluster in self.query_expansion_words:
+				if term in cluster:
+					expanded_query.extend(cluster)
+			if not in_cluster:
+				expanded_query.append(term)
+		print(expanded_query)
+
+		expanded_query = " ".join(expanded_query)
+		tf_idf_query = self.tfidf_vec.transform([expanded_query]).toarray()[0]
 
 
 		# print("make similarity dict")
 		similarity_dict = {}
 		for car in truncated_list_by_size:
 			car_index = self.cars_reverse_index[car]
-			print(self.doc_by_vocab[car_index].T.shape)
-			print('tf idf', tf_idf_query.shape)
 			sim = get_sim(self.doc_by_vocab[car_index].T, tf_idf_query)
 			similarity_dict[car] = sim
 
--- kmeans.py
+++ kmeans.py
@@ -13,13 +13,18 @@ doc_by_vocab_mat = np.load("data/doc_by_vocab.npy")
 model = KMeans(n_clusters=NUM_CLUSTERS, init='k-means++', max_iter=100, n_init=1)
 model.fit(doc_by_vocab_mat)
 
-print("Top terms per cluster:")
+# print("Top terms per cluster:")
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
-
+relevant_keywords = []
 for i in range(NUM_CLUSTERS):
-    print ("Cluster %d:" % i)
+    # print ("Cluster %d:" % i)
+    cluster = set()
     for ind in order_centroids[i, :10]:
-        print (' %s' % index_to_vocab_dict[ind])
+        # print (' %s' % index_to_vocab_dict[ind])
+        cluster.add(index_to_vocab_dict[ind])
     print
+    relevant_keywords.append(cluster)
 
 # output [ sets(keywords) ] to go to a pre-mapped list in search.py
+with open("data/query_expansion_words.pkl", "wb+") as file:
+		pickle.dump(relevant_keywords, file)
